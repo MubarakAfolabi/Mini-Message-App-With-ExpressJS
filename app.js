350d03a990a5c6925cdced9fa8190337d788cea1
MubarakAfolabi <mubarakbolu150@gmail.com>
2025-09-17 18:13:58 +0100

    Handle POST requests to get data from form

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const path = require("node:path");
+const indexRouter = require("./routes/index.js");
+const formRouter = require("./routes/form.js");
 const app = express();
 
-app.set("views", path.join(__dirname), "views");
+app.use(express.urlencoded({ extended: true }));
+
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
+app.use("/new", formRouter);
 
 const PORT = 3000;
 
